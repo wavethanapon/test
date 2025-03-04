bd9918612b00b633e8b499f0564354a28a22078c
wavethanapon <wave082d@gmail.com>
2025-03-04 18:00:45 +0700

    Message แก้ให้loginใช้ได้

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { DataTableComponent } from './data-table/data-table.component';
-
-
-
+import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, // สำคัญ: ต้องมี standalone: true
-  imports: [DataTableComponent], // เพิ่ม DataTableComponent ใน imports
+  imports: [DataTableComponent,RouterOutlet,LoginComponent], // เพิ่ม DataTableComponent ใน imports
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-data-table',
   standalone: true,
