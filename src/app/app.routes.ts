2eb0a2b82e78753413232a623e2809e83009d206
wavethanapon <wave082d@gmail.com>
2025-03-05 12:53:39 +0700

    Message login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
+import { DataTableComponent } from './data-table/data-table.component';
+import { LoginComponent } from './login/login.component';
 
-
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'table', component: DataTableComponent },
+  {path: 'login',component: LoginComponent}
+];
--- src/app/data-table/data-table.component.ts
+++ src/app/data-table/data-table.component.ts
@@ -6,9 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./data-table.component.css']
 })
 export class DataTableComponent {
-  tableData = [
-    { studentId: '60010001', fullName: 'สมชาย ใจดี', nickName: 'ชาย', generation: '60', email: 'somchai@example.com', phoneNumber: '0812345678', points: 10 },
-    { studentId: '60010002', fullName: 'สมหญิง จริงใจ', nickName: 'หญิง', generation: '60', email: 'somying@example.com', phoneNumber: '0898765432', points: 15 },
-    // เพิ่มข้อมูลเพิ่มเติมได้ตามต้องการ
-  ];
 }
